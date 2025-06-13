02ed16db62c73af2c7ec41e0dee59c2e98209b23
alon2004 <123461435+alon2004@users.noreply.github.com>
2025-06-13 16:59:46 +0300

    Update esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [32.09006216694722, 34.80270808579056],
+            center: [34.80270808579056, 32.09006216694722],
             container: 'map',
             map: map,
             zoom: 20
